0c2fb24b234f8426b7f9e9c10eb90d5b30c05565
sofisl <55454395+sofisl@users.noreply.github.com>
2020-04-15 09:22:23 -0700

    Update and rename electronTesting.js to index.js

---
+++ index.js
@@ -0,0 +1,101 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const {google} = require('googleapis');
+const sampleClient = require('./sampleclient');
+const fs = require('fs')
+const readline = require('readline');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+app.on('ready', main)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+const youtube = google.youtube({
+  version: 'v3',
+  auth: sampleClient.oAuth2Client,
+});
+
+async function runSample(fileName) {
+  const fileSize = fs.statSync(fileName).size;
+  const res = await youtube.videos.insert(
+    {
+      part: 'id,snippet,status',
+      notifySubscribers: false,
+      requestBody: {
+        snippet: {
+          title: 'Node.js YouTube Upload Test',
+          description: 'Testing YouTube upload via Google APIs Node.js Client',
+        },
+        status: {
+          privacyStatus: 'private',
+        },
+      },
+      media: {
+        body: fs.createReadStream(fileName),
+      },
+    },
+    {
+      // Use the `onUploadProgress` event from Axios to track the
+      // number of bytes uploaded to this point.
+      onUploadProgress: evt => {
+        const progress = (evt.bytesRead / fileSize) * 100;
+        readline.clearLine(process.stdout, 0);
+        readline.cursorTo(process.stdout, 0, null);
+        process.stdout.write(`${Math.round(progress)}% complete`);
+      },
+    }
+  );
+  console.log('\n\n');
+  console.log(res.data);
+  return res.data;
+}
+
+function main() {
+  sampleClient
+    .authenticate([
+      'https://www.googleapis.com/auth/youtube.upload',
+      'https://www.googleapis.com/auth/youtube',
+    ])
+    .then(() => runSample(path.join((__dirname), "../../../../Downloads/testCoverage.mp4")))
+    .catch(console.error);
+}
